ffd29f1b52682178c2190c0a36660c60e3560df9
Zain Raza <urboyzain@gmail.com>
2020-10-10 23:17:35 -0400

    added memes

--- ALPHABOT.py
+++ ALPHABOT.py
@@ -26,9 +26,11 @@ async def on_message(message):
 
     await client.process_commands(message)
 
+
 @client.command()
 async def source(ctx):
-    await ctx.send("THE SOURCE CODE FOR THE MOST ALPHA BOT DISCORD: https://github.com/zainaraza43/ALPHA-BOT")
+    await ctx.send("THE SOURCE CODE FOR THE MOST ALPHA DISCORD BOT! WIN !!: https://github.com/zainaraza43/ALPHA-BOT")
+
 
 @client.command(aliases=['streamtitle'])
 async def title(ctx):
@@ -58,5 +60,37 @@ async def title(ctx):
                  "GENETIC ANOMALY BULLY TOP PLAYER IN WAY !! MOVE !! UNRANKED TO CHALLENGER TOP LANE !!!"]
     await ctx.send(f'{random.choice(responses)}')
 
+@client.command()
+async def meme(ctx):
+    responses = [
+        "https://cdn.discordapp.com/attachments/421865297285480460/764683875284418574/eyes.png",
+        "https://tenor.com/view/tyler1-loltyler1-screaming-dead-wtf-gif-17500255",
+        "https://cdn.discordapp.com/attachments/421865297285480460/764684688852779068/tyler.png",
+        "https://cdn.discordapp.com/attachments/421865297285480460/764684769312636958/pz1uVcP_d.png",
+        "https://cdn.discordapp.com/attachments/421865297285480460/764684904759558174/unknown.png",
+        "https://cdn.discordapp.com/attachments/421865297285480460/764684996790976532/rerrrr.PNG",
+        "https://cdn.discordapp.com/attachments/421865297285480460/764684985931661312/fadsfff.PNG",
+        "https://cdn.discordapp.com/attachments/421865297285480460/764685091003695144/unknown.png",
+        "https://cdn.discordapp.com/attachments/421865297285480460/764685209337200670/unknown.png",
+        "https://cdn.discordapp.com/attachments/421865297285480460/764685291117477898/unknown.png",
+        "https://cdn.discordapp.com/attachments/421865297285480460/764685347643719730/unknown.png",
+        "https://tenor.com/view/tyler-booty-tyler1-t1-alpha-gif-14526618",
+        "https://cdn.discordapp.com/attachments/421865297285480460/764685540955783198/2afb06a434ccef228c227e556bc7e28e96f7f009.png",
+        "https://cdn.discordapp.com/attachments/421865297285480460/764685609021079582/DcMuMMEWAAAuwwC.png",
+        "https://media.discordapp.net/attachments/421865297285480460/764685946154254427/323538814021201.png",
+        "https://cdn.discordapp.com/attachments/421865297285480460/764685992137326602/D6yEXwZWkAEl97F.png",
+        "https://cdn.discordapp.com/attachments/421865297285480460/764686030879850526/w953wj3wdbi11.png",
+        "http://community.tacticsoft.net/uploads/db9473/original/3X/d/1/d17c9e5edbff313af8a9ee4442cec18f0b50a0ea.gif",
+        "https://cdn.discordapp.com/attachments/421865297285480460/764686163767197696/unknown.png",
+        "https://tenor.com/view/tyler1-food-drop-have-some-oops-food-spill-gif-14542838",
+        "https://tenor.com/view/tyler1-funny-meme-sad-tyler1meme-gif-18519966",
+        "https://tenor.com/view/tyler1-funny-meme-sad-tyler1meme-gif-18519966",
+        "https://cdn.discordapp.com/attachments/421865297285480460/764686444035178537/unknown.png",
+        "https://tenor.com/view/league-of-legends-tyler1backflip-fail-gif-14420932",
+        "https://cdn.discordapp.com/attachments/421865297285480460/764686534585745408/unknown.png",
+        "https://cdn.discordapp.com/attachments/421865297285480460/764686559345115166/vny6krrv5to51.png",
+        "https://cdn.discordapp.com/attachments/421865297285480460/764686909263314974/18KxlZ0.png"
+    ]
+    await ctx.send(f'{random.choice(responses)}')
 
 client.run(TOKEN)
