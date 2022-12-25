555014ae09f7c6adef1e80dba6f948c71493db47
zainaraza43 <urboyzain@gmail.com>
2022-12-25 00:15:57 -0500

    opgg command added

---
+++ src/commands/tools/opgg.js
@@ -0,0 +1,40 @@
+const { SlashCommandBuilder } = require('discord.js');
+const { execute } = require('./ping');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+    .setName('opgg')
+    .setDescription('Look up a League of Legends player on OP.GG')
+    .addStringOption(option =>
+        option.setName('region')
+        .setDescription('The region of the player you want to search for')
+        .setRequired(true)
+        .addChoices(
+            { name: 'NA', value: 'na' },
+            { name: 'EUW', value: 'euw' },
+            { name: 'KR', value: 'kr' },
+            { name: 'EUNE', value: 'eune' },
+            { name: 'OCE', value: 'oce' },
+            { name: 'JP', value: 'jp' },
+            { name: 'BR', value: 'br' },
+            { name: 'LAS', value: 'las' }, 
+            { name: 'LAN', value: 'lan' },
+            { name: 'RU', value: 'ru' },
+            { name: 'TR', value: 'tr' },
+        )
+    )
+    .addStringOption(option =>
+        option.setName('name')
+        .setDescription('The summoner name of the player')
+        .setRequired(true)
+    ),
+    async execute(interaction, client) {
+        const message = await interaction.deferReply({
+            fetchReply: true
+        });
+        const newMessage = `https://www.op.gg/summoners/${interaction.options.get('region').value}/${interaction.options.get('name').value.replaceAll(' ', '+')}`
+        await interaction.editReply({
+            content: newMessage
+        });
+    }
+}
