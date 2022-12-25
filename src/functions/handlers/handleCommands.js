2751779bda97f1f3221e7d94253d763598ea876b
zainaraza43 <urboyzain@gmail.com>
2022-12-24 22:49:43 -0500

    basic ping command working!

--- src/functions/handlers/handleCommands.js
+++ src/functions/handlers/handleCommands.js
@@ -1,7 +1,9 @@
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
 const fs = require('fs');
 
-module.exports = (clients) => {
-    clients.handleCommands = async() => {
+module.exports = (client) => {
+    client.handleCommands = async() => {
         const commandFolders = fs.readdirSync('./src/commands');
         for (const folder of commandFolders) {
             const commandFiles = fs
@@ -13,8 +15,20 @@ module.exports = (clients) => {
                 const command = require(`../../commands/${folder}/${file}`);
                 commands.set(command.data.name, command);
                 commandArray.push(command.data.toJSON());
-                console.log(`Commmand: ${command.data.name} has registered.`)
+                console.log(`Commmand: ${command.data.name} has registered.`);
             }
         }
-    }
-}
+
+        const rest = new REST({ version: '9' }).setToken(process.env.DISCORD_TOKEN);
+        try {
+            console.log("Started refreshing application (/) commands.");
+
+            await rest.put(Routes.applicationCommands(process.env.CLIENT_ID), {
+                body: client.commandArray,
+            });
+            console.log("Successfully reloaded application (/) commands.");
+        } catch (error) {
+            console.error(error);
+        }
+    };
+};
